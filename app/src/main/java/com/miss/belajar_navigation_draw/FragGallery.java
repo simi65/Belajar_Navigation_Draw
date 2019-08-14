e1d433fae0a923d93b6423efd6e74f8825b478d1
MISBAH <misbah@MacBook-Pro-1.local>
2019-08-14 14:30:03 +0700

    Initial commit

---
+++ app/src/main/java/com/miss/belajar_navigation_draw/FragGallery.java
@@ -0,0 +1,31 @@
+package com.miss.belajar_navigation_draw;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragGallery extends Fragment {
+
+
+    public FragGallery() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_gallery, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/miss/belajar_navigation_draw/FragTools.java
@@ -0,0 +1,31 @@
+package com.miss.belajar_navigation_draw;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragTools extends Fragment {
+
+
+    public FragTools() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_tools, container, false);
+    }
+
+}
